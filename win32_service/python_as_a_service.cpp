b0ca75026b96fdbfd27ba634be965125f805a3fd
Siu Kwan Lam <michael.lam.sk@gmail.com>
2015-12-08 18:09:11 -0600

    Add win32 service C code

---
+++ win32_service/python_as_a_service.cpp
@@ -0,0 +1,113 @@
+
+#pragma comment(lib, "advapi32.lib")
+
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SERVICE_NAME "PythonAsAService"
+#define SLEEP_TIME 0
+#define LOGFILE "C:\\.python_as_a_service.log"
+#define MY_COMMAND "C:\\.python_as_a_service.bat"
+
+SERVICE_STATUS ServiceStatus;
+SERVICE_STATUS_HANDLE hStatus;
+
+void ServiceMain(int argc, char** argv);
+void ControlHandler(DWORD request);
+int InitService();
+void WorkerLoop() ;
+
+
+void main()
+{
+    SERVICE_TABLE_ENTRY ServiceTable[2];
+    ServiceTable[0].lpServiceName = SERVICE_NAME;
+    ServiceTable[0].lpServiceProc = (LPSERVICE_MAIN_FUNCTION)ServiceMain;
+
+    ServiceTable[1].lpServiceName = NULL;
+    ServiceTable[1].lpServiceProc = NULL;
+    // Start the control dispatcher thread for our service
+    StartServiceCtrlDispatcher(ServiceTable);
+}
+
+void WorkerLoop() {
+    system(MY_COMMAND " >> " LOGFILE);
+}
+
+
+void ServiceMain(int argc, char** argv)
+{
+    int error;
+
+    ServiceStatus.dwServiceType        = SERVICE_WIN32;
+    ServiceStatus.dwCurrentState       = SERVICE_START_PENDING;
+    ServiceStatus.dwControlsAccepted   = SERVICE_ACCEPT_STOP | SERVICE_ACCEPT_SHUTDOWN;
+    ServiceStatus.dwWin32ExitCode      = 0;
+    ServiceStatus.dwServiceSpecificExitCode = 0;
+    ServiceStatus.dwCheckPoint         = 0;
+    ServiceStatus.dwWaitHint           = 0;
+
+    hStatus = RegisterServiceCtrlHandler(
+		SERVICE_NAME,
+		(LPHANDLER_FUNCTION)ControlHandler);
+    if (hStatus == (SERVICE_STATUS_HANDLE)0)
+    {
+        // Registering Control Handler failed
+        return;
+    }
+    // Initialize Service
+    error = InitService();
+    if (error)
+    {
+		// Initialization failed
+        ServiceStatus.dwCurrentState       = SERVICE_STOPPED;
+        ServiceStatus.dwWin32ExitCode      = -1;
+        SetServiceStatus(hStatus, &ServiceStatus);
+        return;
+    }
+    // We report the running status to SCM.
+    ServiceStatus.dwCurrentState = SERVICE_RUNNING;
+    SetServiceStatus (hStatus, &ServiceStatus);
+
+    // The worker loop of a service
+    while (ServiceStatus.dwCurrentState == SERVICE_RUNNING)
+	{
+        WorkerLoop();
+		Sleep(SLEEP_TIME);
+	}
+    return;
+}
+
+// Service initialization
+int InitService()
+{
+	return 0;
+}
+
+// Control handler function
+void ControlHandler(DWORD request)
+{
+    switch(request)
+    {
+        case SERVICE_CONTROL_STOP:
+            ServiceStatus.dwWin32ExitCode = 0;
+            ServiceStatus.dwCurrentState  = SERVICE_STOPPED;
+            SetServiceStatus (hStatus, &ServiceStatus);
+            return;
+
+        case SERVICE_CONTROL_SHUTDOWN:
+            ServiceStatus.dwWin32ExitCode = 0;
+            ServiceStatus.dwCurrentState  = SERVICE_STOPPED;
+            SetServiceStatus (hStatus, &ServiceStatus);
+            return;
+
+        default:
+            break;
+    }
+
+    // Report current status
+    SetServiceStatus (hStatus,  &ServiceStatus);
+
+    return;
+}
